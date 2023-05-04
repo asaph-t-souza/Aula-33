f5e48fe7cc6ece4a618174e59621f303f8636665
asaph-t-Souza <asaph.net@gmail.com>
2023-05-03 21:36:01 -0300

    Final

--- many-to-many/src/main/java/com/t3/manytomany/entity/Group.java
+++ many-to-many/src/main/java/com/t3/manytomany/entity/Group.java
@@ -1,5 +1,8 @@
 package com.t3.manytomany.entity;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -13,5 +16,11 @@ public class Group {
     private long id;
 
     private String name;
+
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE},
+    fetch = FetchType.EAGER,
+    mappedBy = "groups"
+    )
+    private Set<Student> students = new HashSet<>();
     
 }
